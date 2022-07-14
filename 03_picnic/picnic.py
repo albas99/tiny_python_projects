00e2884c18bd64f5d2d8fa455140fc9ae188f4b1
Faisal Albasu <albasfaisal@yandex.ru>
2022-07-14 15:14:00 +0400

    add optional argument and logic for sorting items

---
+++ 03_picnic/picnic.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Going on a picnic"""
+import argparse
+
+
+def get_args():
+    """Get CLI arguments/parameters"""
+    parser = argparse.ArgumentParser(
+        description="Store arguments",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        'items',
+        metavar='str',
+        help='Picnic items',
+        nargs='+'
+    )
+    parser.add_argument(
+        '-s',
+        '--sorted',
+        help='Sort items',
+        action='store_true'
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    """Main function -> takes arguments from CLI and prints out the sentence of what to bring"""
+    args = get_args()
+
+    items = args.items
+    if args.sorted:
+        items.sort()
+
+    if len(items) == 1:
+        result = f"You are bringing {items[0]}."
+        print(result)
+    if len(items) == 2:
+        result = f"You are bringing {items[0]} and {items[1]}."
+        print(result)
+    if len(items) > 2:
+        stuff = ', '.join(items[:-1])
+        result = f"You are bringing {stuff}, and {items[-1]}."
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
