b1a851e3d31a4a4552fe1e7c05a123833276803a
Faisal Albasu <albasfaisal@yandex.ru>
2022-07-28 18:25:43 +0400

    command line with vowels: passed

--- 08_apples_and_bananas/apples.py
+++ 08_apples_and_bananas/apples.py
@@ -13,38 +13,27 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Apples and Bananas',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
+    parser.add_argument('text',
                         metavar='str',
-                        help='A positional argument')
+                        help='Text input')
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
+    parser.add_argument('-v',
+                        '--vowel',
+                        help='Vowel to be replaced with',
                         metavar='str',
                         type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
+                        default='a',
+                        choices=['a', 'e', 'i', 'o', 'u'])
 
     parser.add_argument('-f',
                         '--file',
-                        help='A readable file',
+                        help='A readable file input',
                         metavar='FILE',
                         type=argparse.FileType('rt'),
                         default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
 
     return parser.parse_args()
 
@@ -54,17 +43,21 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    text = args.text.lower()
+    vowel = args.vowel
+    file = args.file
+    vowels = ['a', 'e', 'i', 'o', 'u']
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    if file:
+        for line in file:
+            for char in line:
+                if char in vowels:
+                    print(line.replace(char, vowel))
+
+    for char in text:
+        if char in vowels:
+            text = text.replace(char, vowel)
+    print(text)
 
 
 # --------------------------------------------------
